0cfb4d51f1c5d59937ce7a2685543e4b6d40dea3
Kristo Baricevic <108290619+kristo-baricevic@users.noreply.github.com>
2023-08-14 16:31:39 -0400

    Initial commit

---
+++ src/setup/steps/hydrateRepositorySettings.ts
@@ -0,0 +1,38 @@
+import { Octokit } from "octokit";
+
+import { InputValues } from "../../shared/inputs.js";
+
+type HydrateRepositoryValues = Pick<
+	InputValues,
+	"description" | "owner" | "repository"
+>;
+
+export async function hydrateRepositorySettings(
+	octokit: Octokit,
+	{ description, owner, repository }: HydrateRepositoryValues,
+) {
+	await octokit.rest.repos.update({
+		allow_auto_merge: true,
+		allow_rebase_merge: false,
+		allow_squash_merge: true,
+		default_branch: "main",
+		delete_branch_on_merge: true,
+		description,
+		has_wiki: false,
+		owner,
+		repo: repository,
+		security_and_analysis: {
+			advanced_security: {
+				status: "enabled",
+			},
+			secret_scanning: {
+				status: "enabled",
+			},
+			secret_scanning_push_protection: {
+				status: "enabled",
+			},
+		},
+		squash_merge_commit_message: "PR_BODY",
+		squash_merge_commit_title: "PR_TITLE",
+	});
+}
